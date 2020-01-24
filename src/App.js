47f2c3333c9b5b2ffef3df9b99b778d6064f8835
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-01-24 16:57:22 -0300

    agrego el form ejercicio 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-     <form className="formulario">
+class Comentario extends  React.Component{
+  render(){
+    return(
       <div>
-        <label  for="input-nombre">Nombre</label>
+         <h1>{this.props.nombre}</h1>
+         <p>{this.props.comentario}</p>
+      </div>
+    )
+  }
+}
 
-        <input type="text" name="nombre" id="input-nombre" />
-      </div>
-      <div>
-        <label for="input-solucion">Ideas</label>
-        <textarea ></textarea>
-      </div>
-      <button  type="submit" id="btn-enviar">ENVIAR</button>
-    </form>
-  );
+function Formulario(props){
+  return(
+    <form className="formulario" onSubmit={props.handleSubmit}>
+        <div className="contenedor">
+          <label  htmlFor="input-nombre">Nombre</label>
+          <input type="text" name="nombre" id="input-nombre" autoComplete="off" />
+        </div>
+        <div className="contenedor">
+          <label htmlFor="input-comentario">Comentario</label>
+          <textarea id="input-comentario"></textarea>
+        </div>
+      <button  type="submit" id="btn-enviar" >ENVIAR</button>
+  </form>
+  )
+}
+
+class App extends React.Component{
+
+  state = {
+    nombre: '',
+    comentario: ''
+  }
+
+  handleSubmit = (event) =>{
+    event.preventDefault()
+    let nombre = document.querySelector('#input-nombre').value;
+    let comentario = document.querySelector('#input-comentario').value;
+
+    this.setState({
+       nombre: nombre,
+      comentario: comentario });
+
+    
+  }
+
+  render(){
+    return(
+    
+      
+   <div>
+      <Comentario nombre={this.state.nombre} comentario={this.state.comentario} />
+      <Formulario handleSubmit={this.handleSubmit}/>
+    </div>
+    )
+  }
 }
 
 export default App;
